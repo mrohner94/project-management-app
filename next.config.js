615cf4eee6871fd8a942ecda008e68f40cd74f2b
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-05-21 14:09:13 -0400

    Fix some things

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["bcrypt"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
